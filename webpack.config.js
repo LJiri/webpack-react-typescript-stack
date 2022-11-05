4606e6b9c3378543d6727c284c704c5ee8862b03
Jiří Laudát <j.laudat@email.cz>
2022-11-05 19:06:01 +0100

    fix code by prettier

--- webpack.config.js
+++ webpack.config.js
@@ -5,73 +5,73 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
+    const isProduction = argv.mode === "production";
 
-  return {
-    context: path.resolve(__dirname, "./src"),
-    entry: "./index.tsx",
-    output: {
-      path: path.join(__dirname, "./dist"),
-      filename: isProduction ? "[name].[contenthash].js" : "[name].js",
-    },
-    resolve: {
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          exclude: /node_modules/,
-          use: [
-            "babel-loader",
-            {
-              loader: "ts-loader",
-              options: {
-                onlyCompileBundledFiles: true,
-              },
-            },
-          ],
+    return {
+        context: path.resolve(__dirname, "./src"),
+        entry: "./index.tsx",
+        output: {
+            path: path.join(__dirname, "./dist"),
+            filename: isProduction ? "[name].[contenthash].js" : "[name].js",
         },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ["babel-loader"],
+        resolve: {
+            extensions: [".js", ".jsx", ".ts", ".tsx"],
         },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        "babel-loader",
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                onlyCompileBundledFiles: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.(png|jpg|jpeg|gif|woff|woff2|eot|ttf|svg)$/,
+                    type: "asset",
+                },
+            ],
         },
-        {
-          test: /\.(png|jpg|jpeg|gif|woff|woff2|eot|ttf|svg)$/,
-          type: "asset",
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./templates/index.html",
+                minify: false,
+            }),
+            // extract all styles
+            new MiniCssExtractPlugin({
+                filename: isProduction ? "[name].[contenthash].css" : "[name].css",
+            }),
+            new CleanWebpackPlugin(),
+        ],
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    // disable webpack licence file
+                    extractComments: false,
+                }),
+            ],
         },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./templates/index.html",
-        minify: false,
-      }),
-      // extract all styles
-      new MiniCssExtractPlugin({
-        filename: isProduction ? "[name].[contenthash].css" : "[name].css",
-      }),
-      new CleanWebpackPlugin(),
-    ],
-    optimization: {
-      minimizer: [
-        new TerserPlugin({
-          // disable webpack licence file
-          extractComments: false,
-        }),
-      ],
-    },
-  };
+    };
 };
